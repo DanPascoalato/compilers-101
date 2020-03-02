7c9d534ba6a1ec79d7d692a9973a228061f3ace0
iobruno <bruno.ariev@gmail.com>
2020-03-01 21:38:04 -0300

    impl. lexicalparser for numbers in c++

--- src/LexicalParser.cpp
+++ src/LexicalParser.cpp
@@ -1,11 +1,81 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
-int main() {
+bool is_decimal_symbol(char);
+bool is_negative_symbol(char);
+bool is_whitespace_or_eol(char);
+vector<string> parse_numbers_from(string);
 
-    string message = "Hello World";
-    cout << message;
+int main()
+{
+    const string message = "abc@  127.32 -28";
+    const vector<string> numbers = parse_numbers_from(message);
+
+    for (int i = 0; i < numbers.size(); i++) {
+        cout << numbers[i] << endl;
+    }
 
     return 0;
+}
+
+vector<string> parse_numbers_from(string message) {
+    vector<string> numbers;
+    string buffer;
+    bool skip_until_whitespace = false, can_read_decimal = true, can_read_negative = true;
+
+    for (int i = 0; i <= message.length(); i++)
+    {
+        char chr = message[i];
+
+        if (skip_until_whitespace) {
+            if (is_whitespace_or_eol(chr)) {
+                skip_until_whitespace = false;
+                can_read_decimal = can_read_negative = true;
+                buffer = "";
+            }
+            continue;
+        }
+
+        if (isnumber(chr)) {
+            buffer += chr;
+            can_read_negative = false;
+        }
+        else if (is_decimal_symbol(chr) && can_read_decimal) {
+            buffer += chr;
+            can_read_decimal = false;
+            can_read_negative = false;
+        }
+        else if (is_negative_symbol(chr) && can_read_negative) {
+            buffer = chr;
+            can_read_negative = false;
+        }
+        else if (is_whitespace_or_eol(chr)) {
+            if (buffer != "") {
+                numbers.push_back(buffer);
+                buffer = "";
+            }
+            can_read_decimal = can_read_negative = true;
+        }
+        else {
+            skip_until_whitespace = true;
+            buffer = "";
+        }
+    }
+
+    return numbers;
+}
+
+
+bool is_decimal_symbol(char chr) {
+    return chr == '.';
+}
+
+bool is_negative_symbol (char chr) {
+    return chr == '-';
+}
+
+bool is_whitespace_or_eol(char chr) {
+    return chr == ' ' or chr == '\0';
 }
