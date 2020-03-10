d95575db2e0849fb0878e2bab44b842f86b47fa0
iobruno <bruno.ariev@gmail.com>
2020-03-09 22:17:25 -0300

    impl. CppClasses

--- src/CppClasses.cpp
+++ src/CppClasses.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <cstdint>
 
 enum TokenType {
     TEXTO=1,
@@ -7,6 +6,8 @@ enum TokenType {
     REAL=3
 };
 
+
+// Only one of the following can be assigned at the same time
 union Value {
     int i;
     double r;
@@ -18,18 +19,38 @@ class Token {
     public:
         TokenType tokenType;
         Value value;
+
+    Token(TokenType tt, int integerValue) {
+        tokenType = tt;
+        value = Value();
+        value.i = integerValue;
+    }
+
+    Token(TokenType tt, double doubleValue) {
+        tokenType = tt;
+        value = Value();
+        value.r = doubleValue;
+    }
+
+    Token(TokenType tt, char tValue) {
+        tokenType = tt;
+        value = Value();
+        value.t = &tValue;
+    }
+
 };
 
 using namespace std;
 
+
 int main() {
+    Token intToken = Token(INTEIRO, 10);
+    cout << "Token -> Value -> i: " << intToken.value.i << endl;
 
-    Token token;
-    token.tokenType = TEXTO;
-    token.value.i = 10;
-    token.value.r = 3.14;
+    Token doubleToken = Token(REAL, 3.14);
+    cout << "Token -> Value -> r: " << doubleToken.value.r << endl;
 
-    cout << token.tokenType << endl;
-
+    Token charToken = Token(TEXTO, 'z');
+    cout << "Token -> Value -> t: " << *charToken.value.t << endl;
 }
 
