6c462dc9df9d297f2d5f257b5bcd6968da04f1af
iobruno <bruno.ariev@gmail.com>
2020-03-29 03:38:18 -0300

    [WIP] calculator parser

--- src/CalculatorParser.cpp
+++ src/CalculatorParser.cpp
@@ -22,44 +22,61 @@ class Node {
             this->right = right;
         }
 
-        Node(Node* left, double value) : Node(left, value, nullptr) { }
-
-        Node(double value) : Node(nullptr, value, nullptr) { }
+        explicit Node(double value) : Node(nullptr, value, nullptr) { }
 
 };
 
+class Operation {
 
-vector<Node*> sort(vector<Node*> operations) {
-    vector<Node*> sorted;
+    public:
+        char op;
+        Node* left;
+        Node* right;
+
+    Operation(Node* left, char op, Node *right) {
+        this->op = op;
+        this->left = left;
+        this->right = right;
+    }
+
+    Operation(Node* left, char op) : Operation(left, op, nullptr) { }
+
+    explicit Operation(char op) : Operation(nullptr, op, nullptr) { }
+
+};
+
+vector<Operation*> sort(vector<Operation*> operations) {
+    vector<Operation*> sorted;
     sorted.push_back(operations[3]);
-    sorted.push_back(operations[4]);
     sorted.push_back(operations[2]);
     sorted.push_back(operations[1]);
+    sorted.push_back(operations[4]);
     sorted.push_back(operations[0]);
-    sorted.push_back(operations[5]);
     return sorted;
 }
 
-double compute(double left, char op, double right) {
+Node* compute(Node* left, char op, Node* right) {
     double result;
     switch (op) {
-        case '+': result = left + right; break;
-        case '-': result = left - right; break;
-        case '*': result = left * right; break;
-        case '/': result = left / right; break;
-        case '^': result = pow(left, right); break;
+        case '+': result = left->value + right->value; break;
+        case '-': result = left->value - right->value; break;
+        case '*': result = left->value * right->value; break;
+        case '/': result = left->value / right->value; break;
+        case '^': result = pow(left->value, right->value); break;
         default: result = 0.0;
     }
-    return result;
+    return new Node(left->left, result, right->right);
 }
 
 int main() {
     string input = "20 + 32 * 47 * 2 ^ 10 + 130";
     string buffer = "";
+
+    vector<Operation*> operations;
     vector<Node*> nodes;
 
-    Node* current = nullptr;
-    Node* previous = nullptr;
+    Node* node = nullptr;
+    Operation* operation = nullptr;
 
     for(int i = 0, j = 0; i <= input.length(); i++) {
         char chr = input[i];
@@ -68,22 +85,36 @@ int main() {
             buffer += chr;
         }
         else if(is_math_operator(chr) || is_eol(chr)) {
-            previous = current;
-            current = new Node(previous, stod(buffer), nullptr);
+            node = new Node(node, stod(buffer), nullptr);
+            operation = new Operation(node, chr);
 
-            if (!nodes.empty()) {
-                current->left = previous;
-                previous->right = current;
+            if (!operations.empty()) {
+                operations[j - 1]->right = node;
+                nodes[j-1]->right = node;
+                node->left = nodes[j-1];
             }
 
-            nodes.push_back(current);
+            nodes.push_back(node);
+            operations.push_back(operation);
             buffer = "";
             j++;
         }
     }
 
-    vector<Node*> sortedNodes = sort(nodes);
-    cout << "Hello" << endl;
+    vector<Operation*> sortedOps = sort(operations);
+    double result = 0.0;
+
+    for (int i = 0; i < sortedOps.size(); i++) {
+        Node *left = sortedOps[i]->left;
+        Node *right = sortedOps[i]->right;
+
+        Node *resultNode = compute(left, sortedOps[i]->op, right);
+        resultNode->left->right = resultNode;
+        resultNode->right->left = resultNode;
+
+        result += resultNode->value;
+        cout << "Result: " << result << endl;
+    }
 }
 
 
