3bec2ffaeb664fdbb8501a7ace4c3397158f7740
iobruno <bruno.ariev@gmail.com>
2020-04-14 11:37:13 -0300

    impl. function pointer for move_right, move_left, next_char, prev_char

--- src/TuringMachineLike.cpp
+++ src/TuringMachineLike.cpp
@@ -2,39 +2,48 @@
 
 using namespace std;
 
-void fn_select(char instruction) {
-    switch (instruction) {
-        case '>': break;
-        case '<': break;
-        case '+': break;
-        case '-': break;
+typedef void (*fnptr)(const char*);
+
+void move_right(const char* charSeq) {
+    charSeq++;
+}
+
+void move_left(const char* charSeq) {
+    charSeq--;
+}
+
+void next_char(const char* charSeq) {
+    char *buffer = const_cast<char *>(charSeq);
+    (*buffer)++;
+}
+
+void prev_char(const char* charSeq) {
+    char *buffer = const_cast<char *>(charSeq);
+    (*buffer)--;
+}
+
+fnptr fn_select(char op) {
+    void (*fn)(const char*);
+    switch (op) {
+        case '>': fn = &move_right; break;
+        case '<': fn = &move_left; break;
+        case '+': fn = &next_char; break;
+        case '-': fn = &prev_char; break;
         case '(': break;
         case ')': break;
         case '.': break;
         case ',': break;
         case '#': break;
     }
-}
-
-void move_right(char* charSeq) {
-    charSeq++;
-}
-
-void move_left(char* charSeq) {
-    charSeq--;
-}
-
-void next_char(char* charSeq) {
-    (*charSeq)++;
-}
-
-void prev_char(char* charSeq) {
-    (*charSeq)--;
+    return fn;
 }
 
 int main() {
     string expr = "marrocos";
     string instructions = "+[>,]<-[+.<-]";
+
+    fnptr fnptr = fn_select('+');
+    fnptr(expr.c_str());
 
     return 0;
 }
