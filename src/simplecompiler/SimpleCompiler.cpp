1f9754cf0e478ef579571b5db18b76e563b53b9f
iobruno <bruno.ariev@gmail.com>
2020-05-08 00:44:24 -0300

    impl. simple compiler

--- src/simplecompiler/SimpleCompiler.cpp
+++ src/simplecompiler/SimpleCompiler.cpp
@@ -1,47 +1,101 @@
 #include <iostream>
 #include <vector>
-
-#define MAX 3000
+#include <unordered_map>
 
 using namespace std;
+unordered_map<string, string> fn_variables;
 
+enum TokenType {
+    INTEGER=1,
+    VAR=2,
+    OPERATOR=3
+};
 
-bool eval_is_vallid(vector<string> tokens) {
+class Token {
+    public:
+        TokenType type;
+        string var;
+        string op;
+        int value;
+
+        Token(string word) {
+            if (word.find_first_not_of("0123456789") == string::npos) {
+                this->value = stoi(word);
+                this->type = INTEGER;
+            } else if (word.find_first_not_of("+-*/:=") == string::npos) {
+                this->op = word;
+                this->type = OPERATOR;
+            } else {
+                this->var = word;
+                this->type = VAR;
+            }
+        }
+};
+
+typedef vector<Token*> expr;
+
+bool is_valid_eval(expr expression) {
+    auto varname = expression[0]->var;
+    vector<Token*> vargs(expression.begin()+2, expression.end());
+
+    if (varname.length() > 8 || varname.find_first_not_of("abcdefghijklmnopqrstwxyz") != string::npos)
+        return false;
+
+    for (auto arg : vargs) {
+        if (arg->type == VAR) {
+            if (fn_variables.find(arg->var) == fn_variables.end())
+                return false;
+        }
+    }
+    fn_variables[varname] ="value";
     return true;
 }
 
-vector<string> split_expr(string expr) {
-    vector<string> words;
+bool is_valid_eval(vector<expr> expressions) {
+    for (auto expression: expressions) {
+        if (!is_valid_eval(expression))
+            return false;
+    }
+    return true;
+}
+
+expr str_to_expr(string expr_str) {
+    vector<Token*> tokens;
     string word;
 
-    for (auto token : expr) {
+    for (auto token : expr_str) {
         if (token == ' ') {
-            words.push_back(word);
+            tokens.push_back(new Token(word));
             word = "";
-        }
-        else{
+        } else {
             word.push_back(token);
         }
     }
 
-    words.push_back(word);
-    return words;
+    if (!(word != "")) {
+        tokens.push_back(new Token(word));
+    }
+    return tokens;
 }
 
-
 int main() {
+    vector<expr> expressions;
+    string line;
 
-    string expr;
-    
-    while(getline(cin, expr, '\n')) {
-        const char *str = expr.c_str();
-        const vector<string> tokens = split_expr(str);
-        int is_valid_expr = eval_is_vallid(tokens);
-
-        if (is_valid_expr)
-            cout << "OK" << endl;
-        else
-            cout << "Compilation Error" << endl;
+    for (int i = 0; i < 2000; i++) {
+        getline(cin, line);
+        if (line.empty()) {
+            break;
+        }
+        expr expr = str_to_expr(line);
+        expressions.push_back(expr);
     }
+
+    if (is_valid_eval(expressions)) {
+        cout << "OK" << endl;
+    } else {
+        cout << "Compilation Error" << endl;
+    }
+
     return 0;
 }
