495fdbc2235df758b5c4d753f126d87c9f04eed5
iobruno <bruno.pereira@g.globo>
2020-06-13 22:37:36 -0300

    working on verify identifier

--- src/birl/RecursiveDescendantParser.cpp
+++ src/birl/RecursiveDescendantParser.cpp
@@ -5,38 +5,53 @@
 
 using namespace std;
 
-typedef enum {
-    val, def
-} Symbol;
+string line;
+regex varDeclarationPattern(R"((va[lr])\s(\w+)\s*=\s*(.+))");
+regex funcPattern(R"((def) (\w+)\s*(.*)\s*\{\s*)");
+regex ifPattern(R"((if)\s+(.*)\s*\{\s*)");
+regex whilePattern(R"((while)\s+(.*)\s*\{\s*)");
+regex returnPattern(R"((return)\s+(\w+))");
 
-string next_line() {
-    string line;
+typedef enum {val, def} Symbol;
+
+void next_line() {
     getline(cin, line);
-    return line;
+}
+
+
+bool verify_identifier(string id) {
+    regex identifier(R"([A-z][a-z0-9]*)");
+    smatch results;
+    return regex_match(line, results, varDeclarationPattern);
+}
+
+// each line is a block
+void block() {
+    smatch results;
+
+    if (regex_match(line, results, varDeclarationPattern)) {
+        string symbol = results[1];
+        string identifier = results[2];
+        string vargs = results[3];
+
+
+
+    } else if (regex_match(line, results, ifPattern)) {
+        string symbol = results[1];
+        string condition_expr = results[2];
+    } else if (regex_match(line, results, whilePattern)) {
+        string symbol = results[1];
+        string condition_expr = results[2];
+    } else if (regex_match(line, results, funcPattern)) {
+        string symbol = results[1];
+        string identifier = results[2];
+        string vargs = results[3];
+    }
+
 }
 
 int main() {
-    smatch results;
-    map<string, string> terminals;
-    map<string, string> functions;
-
-    regex varDeclarationPattern(R"(va[lr]\s(\w+)\s*=\s*(.+))");
-    regex funcPattern(R"(def (\w+)\s*(.*)\s*\{\s*)");
-    regex ifPattern(R"(if\s+(.*)\s*\{\s*)");
-    regex whilePattern(R"(while\s+(.*)\s*\{\s*)");
-    regex returnPattern(R"(return\s+(\w+))");
-
-    string line = next_line();
-    if (regex_match(line, results, funcPattern)) {
-        terminals[results[1]] = results[2];
-    } else if (regex_match(line, results, varDeclarationPattern)) {
-        functions[results[1]] = results[2];
-    } else if (regex_match(line, results, ifPattern)) {
-        string condition_expr = results[1];
-    } else if (regex_match(line, results, whilePattern)) {
-        string condition_expr = results[1];
-        cout << condition_expr;
-    } else { }
-
+    next_line();
+    block();
     return 0;
 }
